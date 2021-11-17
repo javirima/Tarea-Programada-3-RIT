43ed4ade1d4358861c60763cc4447a026253f08d
Hawk-chan <30329240+natalia0911@users.noreply.github.com>
2021-11-17 16:55:57 -0600

    Bayesianos

--- TP3-RiveraMadrigalJavier-VargasResyesNatalia/clasificadores.py
+++ TP3-RiveraMadrigalJavier-VargasResyesNatalia/clasificadores.py
@@ -3,16 +3,107 @@ import json
 
 global clases
 global totalDocuments
+global bayesianosClases
+global niBayesianos
 totalDocuments = 0
 clases = {}
+bayesianosClases = {}  #Frecuencias de clases por termino
+niBayesianos = {}      #Ni para Frecuencias de clases por termino
+vectoresBayesianos = {}
+
 #clase=[docsClase,centroide,[terminos]]
-def getTotalDocuments(coleccion):
+
+#-----------------------------------Inicio Bayesianos--------------------------------------#
+
+def calQip(termino,clase):
+    global bayesianosClases
+    for listaTermino in  bayesianosClases[clase][1]:
+        if listaTermino[0] == termino:
+            #print((1+niBayesianos[termino]-listaTermino[1])/(2+totalDocuments-listaTermino[1]))
+            return (1+niBayesianos[termino]-listaTermino[1])/(2+totalDocuments-listaTermino[1])
+
+    return 0
+    
+
+def calPip(termino,clase):
+    global bayesianosClases
+    for listaTermino in  bayesianosClases[clase][1]:
+        if listaTermino[0] == termino:
+            #print((1+listaTermino[1]) / (2+bayesianosClases[clase][0]))
+            return (1+listaTermino[1])/(2+bayesianosClases[clase][0])  #(1+nip)/(2+ni)
+    return 0
+
+
+
+def calVectorsPerClassBayesian():
+    global bayesianosClases
+    tempDicc = {} #Para guardar {"Clase": [ [termino1,#], [termino2,#] ]}
+                  #Donde # es la formula (Pip/(1-Pip)) + log((1-qip)/qip)
+    for clase in bayesianosClases:
+        for listaTermino in bayesianosClases[clase][1]:
+            #tempDicc[listaTermino[1]] = []
+            pip = calPip(listaTermino[0],clase)
+            qip = calQip(listaTermino[0],clase)
+            #formule = 
+            #listaTermino[1]
+
+def searchInTerms(termino, postings):
+    #Decir si un termino se encuentra en los terminos de alguna clase en bayesianosClases
+    for post in postings:
+        if (post[0]==termino):
+            return True
+    return False
+
+
+def addFrecInClass(termino, postings):
+    for post in postings:
+        if (post[0]==termino):
+            post[1]+=1
+    return False
+
+def getClassesInfomation(coleccion):
+    global bayesianosClases
     global totalDocuments
+
     for doc in coleccion:
-        totalDocuments = totalDocuments+1
+
+        clase = coleccion[doc]['CLASE']
+        
+        if (clase in bayesianosClases):  #Si la clase ya existe en el dicc
+            bayesianosClases[clase][0] = bayesianosClases[clase][0]+1
+        else:
+            bayesianosClases[clase] = [1,[]]  #Agrega la clase con su frecuencia y lista de terminos
+            
+       
+        
+        for listaTermino in coleccion[doc]["POSTINGS"]:  #recorre lista de postings del doc actual
+            
+            if(searchInTerms(listaTermino[0],bayesianosClases[clase][1])): #Si el termino está en la lista de posting de la clase del doc actual
+                
+                addFrecInClass(listaTermino[0],bayesianosClases[clase][1]) #Agregar la frecuencia 
+                
+            else:
+                bayesianosClases[clase][1].append([listaTermino[0],1])  #agrega una lista de termino y frecuencia en los postings de bayesianosClases
 
 
+            if listaTermino[0] in niBayesianos:
+                niBayesianos[listaTermino[0]] += 1
+                
+            else:
+                niBayesianos[listaTermino[0]] = 1
+
+        totalDocuments = totalDocuments+1  #Contar docs, total np
+
+
+
+#----------------------------------Fin Bayesianos--------------------------------------------#
+
+
+
+        
 def getClasses(coleccion):
+    global clases
+    global totalDocuments
     index = 0
 
     for doc in coleccion:
@@ -23,6 +114,7 @@ def getClasses(coleccion):
             clases[coleccion[doc]['CLASE']] = [1,0,[]]
 
         index+=1
+        totalDocuments = totalDocuments+1  #Contar docs
 
 def sumarCantidad(clase,term):
     for terms in clases[clase][2]:
@@ -78,10 +170,11 @@ def calculateCentroide(key,b,y):
 def rocchio(dir):
     coleccion = json.load(open(dir+'/'+'coleccion.json','r'))
     getClasses(coleccion)
-    getTotalDocuments(coleccion)
+    print(clases)
 
     for i in clases:
         getTerminosClase(i,coleccion) #agrega los terminos de la clase en el dict clases
+        print(clases)
         calcularVector(i,coleccion,0.75,0.25)
         #calculateCentroide(i,0.75,0.25)
 
@@ -89,8 +182,26 @@ def rocchio(dir):
 
     
 
-   
+def main(dir):
+    coleccion = json.load(open(dir+'/'+'coleccion.json','r'))
+    getClassesInfomation(coleccion)
+    #print(bayesianosClases)
+    print()
+    print("----------------------------------------------------------------------------------------------------------------------")
+    print()
+    #print(niBayesianos)
+    calVectorsPerClassBayesian()
 
-dir='C:/Users/javir/Desktop/TEC Javi/RIT/TP3-RiveraMadrigalJavier-VargasResyesNatalia/Index'
 
-rocchio(dir)
+#dir='C:/Users/javir/Desktop/TEC Javi/RIT/TP3-RiveraMadrigalJavier-VargasResyesNatalia/Index'
+dir='D:\\2 SEMESTRE 2021\\RIT\\PROYECTOS\\Proyecto 3\\Tarea-Programada-3-RIT\\TP3-RiveraMadrigalJavier-VargasResyesNatalia\\Index'
+
+#rocchio(dir)
+
+main(dir)
+
+
+
+
+
+
